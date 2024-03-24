24ba95d699162f01d6ee325d30908a368e63d648
MykhailoGrygorenko <osnova2021@gmail.com>
2024-03-24 13:46:20 +0200

    flyweight

---
+++ design patterns/structural/facade.ts
@@ -0,0 +1,26 @@
+
+class Facade{
+    facadeMethdod(): void{
+        new Subsystem1().method();
+        new Subsystem2().method();
+        new Subsystem3().method();
+    }
+}
+
+class Subsystem1{
+    method(): void{
+        
+    }
+}
+
+class Subsystem2{
+    method(): void{
+        
+    }
+}
+
+class Subsystem3{
+    method(): void{
+        
+    }
+}
